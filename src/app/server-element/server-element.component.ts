ad175a07b383cb1dd162d7fcab874dcf90b01076
Magda <magdap.265@gmail.com>
2019-11-21 09:41:17 +0100

    Rozbudowa projektu w ramach której przyswajana jest wiedza dotycząca cyklu życia i funkcji oraz jeg praktyczne zastosowanie.

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,17 +1,70 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, 
+  OnInit, 
+  Input, 
+  OnChanges, 
+  SimpleChange, 
+  DoCheck, 
+  AfterContentInit, 
+  AfterContentChecked, 
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy,
+  ViewChild,
+  ElementRef
+} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css']
 })
-export class ServerElementComponent implements OnInit {
-  @Input() element: {type: string, name: string, content: string};
+export class ServerElementComponent implements 
+  OnInit, 
+  OnChanges, 
+  DoCheck, 
+  AfterContentInit,  
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy {
+  @Input('srvElement') element: {type: string, name: string, content: string};
+  @Input() name: string;
+  @ViewChild('heading', {static: true}) header: ElementRef;
 
-  constructor() { }
+  constructor() {
+    console.log('constructor called');
+  }
+
+  ngOnChanges(changes: SimpleChange)  {
+    console.log('ngOnChanges called');
+    console.log(changes);
+  }
 
   ngOnInit() {
+    console.log('ngOnInit called');
   }
   
+  ngDoCheck () {
+    console.log('ngDoCheck called');
+  }
 
+  ngAfterContentInit() {
+    console.log('ngAfterContentInit called');
+  }
+
+  ngAfterContentChecked() {
+    console.log('ngAfterContentChecked called');
+  }
+
+  ngAfterViewInit() {
+    console.log('ngAfterViewInit called');
+  }
+
+  ngAfterViewChecked() {
+    console.log('ngAfterViewChecked called');
+  }
+  ngOnDestroy() {
+    console.log('ngOnDestroy called');
+  }
+  
 }
